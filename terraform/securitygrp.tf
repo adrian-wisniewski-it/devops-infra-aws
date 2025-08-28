34ee8e89bf09675e2757eae67ba0122fb725ec4b
adrian-wisniewski-it <adrian.wisniewski.it@gmail.com>
2025-08-28 12:19:04 +0000

    Set up ALB with security group and target group, HTTP listener, attach EC2 to target group and add outputs for ALB DNS and EC2 public IPs

--- terraform/securitygrp.tf
+++ terraform/securitygrp.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "devops_sg" {
-  name        = "devops-sg"
-  vpc_id      = aws_vpc.devops_vpc.id
+  name   = "devops-sg"
+  vpc_id = aws_vpc.devops_vpc.id
 
   tags = {
     Name = "devops-sg"
@@ -15,16 +15,40 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "http" {
+resource "aws_vpc_security_group_ingress_rule" "ec2_http_from_alb" {
+  security_group_id            = aws_security_group.devops_sg.id
+  referenced_security_group_id = aws_security_group.devops_alb_sg.id
+  from_port                    = 80
+  ip_protocol                  = "tcp"
+  to_port                      = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "ec2_all" {
   security_group_id = aws_security_group.devops_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+
+resource "aws_security_group" "devops_alb_sg" {
+  name   = "devops-alb-sg"
+  vpc_id = aws_vpc.devops_vpc.id
+
+  tags = {
+    Name = "devops-alb-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_http" {
+  security_group_id = aws_security_group.devops_alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
 }
 
-resource "aws_vpc_security_group_egress_rule" "all" {
-  security_group_id = aws_security_group.devops_sg.id
+resource "aws_vpc_security_group_egress_rule" "alb_all" {
+  security_group_id = aws_security_group.devops_alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
+  ip_protocol       = "-1"
 }
