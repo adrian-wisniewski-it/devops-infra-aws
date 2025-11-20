58d2b67003be69cb1fb70c27aecd20ec7b19ee40
adrian-wisniewski-it <adrian.wisniewski.it@gmail.com>
2025-11-20 15:51:35 +0000

    Enable force_destroy on S3 state bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,6 +4,7 @@ resource "random_id" "suffix" {
 
 resource "aws_s3_bucket" "tf_state_bucket" {
   bucket = "devops-aws-infra-tfstate-${random_id.suffix.hex}"
+  force_destroy = true
 
   tags = {
     Name = "devops-aws-infra-tfstate-bucket"
